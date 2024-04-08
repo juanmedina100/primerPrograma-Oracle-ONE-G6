cc8ab3fe72c6417b8edc99c15fd8b728a3a34324
juanmedina100 <92758289+juanmedina100@users.noreply.github.com>
2024-04-08 09:23:36 -0600

    Concidiones if

---
+++ src/Desiciones.java
@@ -0,0 +1,8 @@
+public class Desiciones {
+    public static void main(String[] args) {
+        int fechaDeLanzamiento = 2022;
+        if (fechaDeLanzamiento >= 2022) {
+            System.out.println("Peliculas mas populares");
+        }
+    }
+}
